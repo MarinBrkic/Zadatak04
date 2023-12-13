aeeacca2a3612ac051a707cb7bef25cdf8676687
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2023-12-13 20:21:23 +0100

    Add files via upload

---
+++ Zadatak04.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Upisi neki broj: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int x = (n - 2) + (n - 1);
+        Console.WriteLine("Broj u fibonaccijevom nizu iznosi: {0}", x);
+    }
+}
